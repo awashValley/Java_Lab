126e2f611b2c72284cfad5c78b85366cadc5ceee
awashValley <hammerson1@gmail.com>
2016-02-09 09:36:44 +0100

    Update debug.java

--- debug.java
+++ debug.java
@@ -1,9 +1,9 @@
 // Qn. Java Error: Should be declared in a file named
        Error: CalculatorWithMemory.java:1: class Calculator is public, should be declared in a file named Calculator.java
               public class Calculator
-        Debug [Stackoverflow-Oliver Charlesworth]: 
+        /*Debug [Stackoverflow-Oliver Charlesworth]: 
               As the error message implies, if you declare a class as public, it needs its own .java file. 
               If you don't want to do that, then don't define it as public. Otherwise, save the java file using the same name as the class name.
-
+       */
 
 
